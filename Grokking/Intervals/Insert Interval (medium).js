01763b1b44375a255bb12cb502b7b903e57dcc31
Amy Kwak <yoomikwak@gmail.com>
2022-07-31 19:45:44 -0700

    done: insert interval

---
+++ Grokking/Intervals/Insert Interval (medium).js
@@ -0,0 +1,43 @@
+// Given a list of non-overlapping intervals sorted by their start time, insert a given interval at the correct position and merge all necessary intervals to produce a list that has only mutually exclusive intervals.
+
+// Input: Intervals=[[1,3], [5,7], [8,12]], New Interval=[4,6]
+// Output: [[1,3], [4,7], [8,12]]
+// Explanation: After insertion, since [4,6] overlaps with [5,7], we merged them into one [4,7].
+
+class Interval {
+  construction(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+}
+
+function insert(intervals, new_interval) {
+  let merged = [],
+    i = 0;
+
+  // skip and add to output all intervals that come before the 'new_interval'
+  while (i < intervals.length && intervals[i].end < new_interval.start) {
+    merged.push(intervals[i]);
+    i++;
+  }
+
+  // merge all intervals that overlap with 'new_interval'
+  while (i < intervals.length && intervals[i].start <= new_interval.end) {
+    new_interval.start = Math.min(intervals[i].start, new_interval.start);
+    new_interval.end = Math.max(intervals[i].end, new_interval.end);
+    i++;
+  }
+
+  // insert the new_interval
+  merged.push(new_interval);
+
+  // add all the remaining intervals to the output
+  while (i < intervals.length) {
+    merged.push(intervals[i]);
+    i++;
+  }
+  return merged;
+}
+
+// Time Complexity: O(N)
+// Space Complexity: O(N)
