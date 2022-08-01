421d617580a4b420160f107c7d15cbedf473941c
Amy Kwak <yoomikwak@gmail.com>
2022-07-31 21:28:38 -0700

    done: merge two sorted lists

--- Grind75/3. Merge Two Sorted Lists.js
+++ Grind75/3. Merge Two Sorted Lists.js
@@ -1,10 +1,6 @@
 // Merge Two Sorted Lists (easy)
 
-// You are given the heads of two sorted linked lists list1 and list2.
-
-// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
-
-// Return the head of the merged linked list.
+// You are given the heads of two sorted linked lists list1 and list2. Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists. Return the head of the merged linked list.
 
 const mergeTwoLists = (list1, list2) => {
   if (!list1 || !list2) return list1 || list2;
