29a150a3612cb303362f647085b78862c77e90eb
Amy Kwak <yoomikwak@gmail.com>
2022-08-01 14:22:39 -0700

    done: ransom note

--- Grind75/16. Ransom Note.js
+++ Grind75/16. Ransom Note.js
@@ -1,6 +1,4 @@
-// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
-
-// Each letter in magazine can only be used once in ransomNote.
+// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise. Each letter in magazine can only be used once in ransomNote.
 
 // Input: ransomNote = "aa", magazine = "aab"
 // Output: true
