989955ad39a880ef810740b86568c547de2457d6
Amy Kwak <yoomikwak@gmail.com>
2022-07-31 20:47:33 -0700

    done: intervals intersection

--- Grokking/Intervals/Intervals Intersection (medium).js
+++ Grokking/Intervals/Intervals Intersection (medium).js
@@ -18,5 +18,33 @@ function merge(intervals_a, intervals_b) {
 
   while (i < intervals_a.length && intervals_b.length) {
     // check if intervals overlap and intervals_a[i]'s start time lies within the other intervals_b[j]
+    a_overlaps_b =
+      intervals_a[i].start >= intervals_b[j].start &&
+      intervals_a[i].start <= intervals_b[j].end;
+
+    // check if intervals overlap and intervals_b[j]'s start time lies within the other intervals_a[i]
+    b_overlaps_a =
+      intervals_b[j].start >= intervals_a[i].start &&
+      intervals_b[j].start <= intervals_a[i].end;
+
+    // store the intersection part
+    if (a_overlaps_b || b_overlaps_a) {
+      result.push(
+        new Interval(
+          Math.max(intervals_a[i].start, intervals_b[j].start),
+          Math.min(intervals_a[i].end, intervals_b[j].end)
+        )
+      );
+    }
+    // move next from the interval which is finish first
+    if (intervals_a[i].end < intervals_b[j].end) {
+      i++;
+    } else {
+      j++;
+    }
   }
+  return result;
 }
+
+// Time Complexity: O(N + M)
+// Space Complexity: O(1) ignoring the space needed for the reuslt list
