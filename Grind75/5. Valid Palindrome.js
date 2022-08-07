daf03591c8c19cb4349a3821a6d36d9d55882d98
Amy Kwak <yoomikwak@gmail.com>
2022-08-07 14:41:53 -0700

    done: valid anagram

---
+++ Grind75/5. Valid Palindrome.js
@@ -0,0 +1,16 @@
+// Valid Palindrome (easy)
+
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+const isPalindrome = (s) => {
+  s = s.replace(/[^a-z0-9]/gi, "").toLowerCase();
+  return s === s.split("").reverse("").join("");
+};
+
+//
--- Grokking/Two Pointers/medium.js
+++ Grokking/Two Pointers/medium.js
@@ -88,7 +88,7 @@ function triplet_sum_close_to_target(arr, targetSum) {
 
 // -----------------------------------------------------------------------------------------------------------------------
 
-// Triplets with Smaller Sum Sum
+// Triplets with Smaller Sum
 
 // Given an array arr of unsorted numbers and a target sum, count all triplets in it such that arr[i] + arr[j] + arr[k] < target where i, j, and k are three different indices. Write a function to return the count of such triplets.
 
