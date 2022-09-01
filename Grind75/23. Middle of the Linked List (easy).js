069f7c38f04607a6b38a36b927ec5760fa91d843
Amy Kwak <yoomikwak@gmail.com>
2022-09-01 10:17:43 -0700

    done

---
+++ Grind75/23. Middle of the Linked List (easy).js
@@ -0,0 +1,20 @@
+// Middle of the Linked List
+
+// Given the head of a singly linked list, return the middle node of the linked list.
+
+// If there are two middle nodes, return the second middle node.
+
+// Input: head = [1,2,3,4,5,6]
+// Output: [4,5,6]
+// Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+const middleNode = (head) => {
+  let slow = head,
+    fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+};
