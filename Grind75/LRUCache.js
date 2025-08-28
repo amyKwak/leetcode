51dc39042c0890869bdf84189fdea2b7d2f8db3f
Amy Kwak <yoomikwak@gmail.com>
2025-08-28 01:45:30 -0700

    lru cache

--- Grind75/LRUCache.js
+++ Grind75/LRUCache.js
@@ -1,39 +1,65 @@
-var LRUCache = function (capacity) {
-  this.capacity = capacity;
-  this.cache = new Map();
-};
+class LRUCache {
+  constructor(capacity) {
+    if (!Number.isInteger(capacity) || capacity <= 0) {
+      throw new Error("capacity must be a positive integer");
+    }
+    this.capacity = capacity;
+    this.map = new Map(); // key -> node
 
-/**
- * @param {number} key
- * @return {number}
- */
-LRUCache.prototype.get = function (key) {
-  if (!this.cache.has(key)) return -1;
-  const value = this.cache.get(key);
-  this.cache.delete(key);
-  this.cache.set(key, value);
-  return value;
-};
-
-/**
- * @param {number} key
- * @param {number} value
- * @return {void}
- */
-LRUCache.prototype.put = function (key, value) {
-  if (this.cache.has(key)) {
-    this.cache.delete(key);
+    // Dummy head/tail to avoid null checks
+    this.head = { prev: null, next: null }; // most-recent end
+    this.tail = { prev: null, next: null }; // least-recent end
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
   }
-  this.cache.set(key, value);
-  if (this.cache.size > this.capacity) {
-    const oldestKey = this.cache.keys().next().value;
-    this.cache.delete(oldestKey);
-  }
-};
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * var obj = new LRUCache(capacity)
- * var param_1 = obj.get(key)
- * obj.put(key,value)
- */
+  get(key) {
+    const node = this.map.get(key);
+    if (!node) return -1;
+    this.#moveToFront(node);
+    return node.value;
+  }
+
+  put(key, value) {
+    let node = this.map.get(key);
+    if (node) {
+      node.value = value;
+      this.#moveToFront(node);
+      return;
+    }
+
+    node = { key, value, prev: null, next: null };
+    this.map.set(key, node);
+    this.#addAfterHead(node);
+
+    if (this.map.size > this.capacity) {
+      this.#evictLeastRecentlyUsed();
+    }
+  }
+
+  // ---- private helpers ----
+  #addAfterHead(node) {
+    node.prev = this.head;
+    node.next = this.head.next;
+    this.head.next.prev = node;
+    this.head.next = node;
+  }
+
+  #removeNode(node) {
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+    // (optional) clean pointers:
+    // node.prev = node.next = null;
+  }
+
+  #moveToFront(node) {
+    this.#removeNode(node);
+    this.#addAfterHead(node);
+  }
+
+  #evictLeastRecentlyUsed() {
+    const lru = this.tail.prev;
+    this.#removeNode(lru);
+    this.map.delete(lru.key);
+  }
+}
