5523a698c9e8f2708631a05b4f0d266caade245c
Amy Kwak <yoomikwak@gmail.com>
2026-04-25 16:19:57 -0700

    Stop tracking Meta folder

--- Meta/isNumber.js
+++
@@ -1,5 +0,0 @@
-var isNumber = function (s) {
-  // Regular expression for a valid number
-  const numberRegex = /^[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?$/;
-  return numberRegex.test(s.trim());
-};
