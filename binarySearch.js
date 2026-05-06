82405a946e6f8b5b61b7f7dfa52efabb00543dbf
Amy Kwak <yoomikwak@gmail.com>
2026-05-05 17:02:15 -0700

    binarySearch

---
+++ binarySearch.js
@@ -0,0 +1,17 @@
+function binarySearch(nums, target) {
+  let l = 0;
+  let r = nums.length - 1;
+
+  while (l <= r) {
+    const mid = Math.floor((l + r) / 2);
+
+    if (nums[mid] === target) return mid;
+    else if (nums[mid] < target) l = mid + 1;
+    else r = mid - 1;
+  }
+
+  return -1;
+}
+
+// Time: O(n log n)
+// Space: (1)
