57a98df80ca03b21dfdfc17563978af1a00af765
Amy Kwak <yoomikwak@gmail.com>
2026-05-24 17:23:53 -0700

    combinationSum

--- Backtracking/combinationSum.js
+++ Backtracking/combinationSum.js
@@ -6,19 +6,18 @@ function combinationSum(candidates, target) {
       result.push([...current]);
       return;
     }
+    if (remaining < 0) return;
 
     for (let i = start; i < candidates.length; i++) {
-      if (candidates[i] > remaining) continue;
-
       current.push(candidates[i]);
       backtrack(i, current, remaining - candidates[i]);
       current.pop();
     }
   }
 
-  backtrack(0, [], result);
+  backtrack(0, [], target);
   return result;
 }
 
-// Time:  O(n^(t/m)) — n candidates, t=target, m=smallest candidate
-// Space: O(t/m)     — max depth of call stack = target / smallest candidate
+// Time Complexity:  O(n^(t/m)) — n candidates, t = target, m = smallest candidate
+// Space Complexity: O(t/m) — maximum recursion depth is target / smallest candidate
