9ba6adfc91525d727fcaf39f50303c83af1d3926
Amy Kwak <yoomikwak@gmail.com>
2026-06-04 12:10:03 -0700

    maxSubArray

---
+++ Arrays/maxSubArray.js
@@ -0,0 +1,14 @@
+function maxSubArray(nums) {
+  let max = nums[0];
+  let curr = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    curr = Math.max(nums[i], curr + nums[i]);
+    max = Math.max(max, curr);
+  }
+
+  return max;
+}
+
+// Time: O(n)
+// Space: O(1)
